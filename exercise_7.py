fabea7b784162e06a128ce06c68edaa3d99d24ca
madm0nkey <coralfruit@gmail.com>
2016-11-28 23:17:08 +0300

    Добавлено решение задания 7

--- exercise_7.py
+++ exercise_7.py
@@ -1,6 +1,5 @@
 import pytest
-import time
-from selenium import webdriver
+from selenium import webdriver, common
 
 
 @pytest.fixture
@@ -15,32 +14,23 @@ def test_admin_check_sections(driver):
     driver.find_element_by_name('username').send_keys('admin')
     driver.find_element_by_name('password').send_keys('admin')
     driver.find_element_by_name('login').click()
-    # time.sleep(2)
-    sections = driver.find_elements_by_css_selector('#box-apps-menu li')
+
+    menu_sections = driver.find_elements_by_css_selector('#box-apps-menu li')
     i = 0
-    while i < len(sections):
-        menu_apps = driver.find_element_by_css_selector('#box-apps-menu')
-        app = menu_apps.find_elements_by_xpath("//li[@id='app-']/a")
-        app[i].click()
-        time.sleep(1)
-        ul = driver.find_elements_by_css_selector('li#app- ul')
-        if ul:
-            # print(ul[0])
+    while i < len(menu_sections):
+        driver.find_elements_by_xpath("//li[@id='app-']/a")[i].click()
+        driver.find_element_by_css_selector('td#content h1')
+
+        try:
+            driver.find_element_by_css_selector('li#app- ul')
+        except common.exceptions.NoSuchElementException:
+            pass
+        else:
+            pass
+            section_items = driver.find_elements_by_css_selector('#app- li')
             j = 0
-            print(len(driver.find_elements_by_css_selector('#app- li')))
-            print(driver.find_elements_by_css_selector('#app- li'))
-            while j < len(driver.find_elements_by_css_selector('#app- li')):
-                ul1 = driver.find_element_by_css_selector('li#app-')
-                ul1.find_elements_by_xpath("./ul/li/a")[j].click()
-                time.sleep(2)
+            while j < len(section_items):
+                driver.find_elements_by_css_selector('#app- li a')[j].click()
+                driver.find_element_by_css_selector('td#content h1')
                 j += 1
         i += 1
-
-    # only main elements
-    # sections = driver.find_elements_by_css_selector('#box-apps-menu li')
-    # i = 0
-    # while i < len(sections):
-    #     menu_apps = driver.find_element_by_css_selector('#box-apps-menu')
-    #     menu_apps.find_elements_by_xpath("//li[@id='app-']/a")[i].click()
-    #     time.sleep(1)
-    #     i += 1
